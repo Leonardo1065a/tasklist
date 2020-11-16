fa390ce14b70ffa33fe663493d46e7c34736d06d
Leonardo <leonardo.osilva@mv.com.br>
2020-11-15 23:22:39 -0300

    configurando para deply

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname  + '/dist'));
+app.use(express.static(__dirname  + '/dist/tasklist/'));
 
-app.get('/*', function(req, res){
-    res.sendFile(__dirname + '/dist/index.html')
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/dist/tasklist/index.html');
 });
 
-app.listen(4200);
+app.listen(process.env.PORT || 4200);
